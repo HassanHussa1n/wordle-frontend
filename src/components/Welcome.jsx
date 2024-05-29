c18e6021ad8b65fe08bd6af8c45e26823671c0aa
HassanHussa1n <hassanmehmodhussain@gmail.com>
2024-05-29 14:11:37 +0200

    added data to welcome and created random word

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const Welcome = () => {
+function Welcome() {
+  const [word, setWord] = useState(null);
+
+
+  useEffect(() => {
+    const fetchWords = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/words");
+        console.log(response.data.data)
+        const fetchedWords = response.data.data.map(wordle => wordle.word);
+        const randomWord = fetchedWords[Math.floor(Math.random()*fetchedWords.length)]
+        setWord(randomWord)
+      } catch (error) {
+        console.error("Error fetching the word: ", error);
+      }
+    };
+
+    fetchWords();
+  }, [setWord]);
+
+
   return (
     <div>
       <h1>Welcome!</h1>
       <p>You have successfully logged in.</p>
+      {word && <div> Word is: {word} </div>}
     </div>
   );
-}; 
+}
 
 export default Welcome;
